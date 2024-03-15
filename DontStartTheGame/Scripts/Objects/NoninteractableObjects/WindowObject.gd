a6377d9f04fdcf93d2c9bd4f69f02197685c1103
TheItsyBitsySpider <k2intern1@madarhome.com>
2024-03-15 16:52:08 -0500

    feat: Floor 3
    Adds in the 3rd floor along with the needed items & rules within it
    
    Also added Duck SFX, and created an Objects folder for object sprites

--- DontStartTheGame/Scripts/Objects/NoninteractableObjects/WindowObject.gd
+++ DontStartTheGame/Scripts/Objects/NoninteractableObjects/WindowObject.gd
@@ -1,7 +1,9 @@
 extends StaticBody2D
 
 @export var description := "None of the rules say defenestration isn’t allowed."
+@export var broken_window_sprite: Resource
 @onready var window_sfx := $SFX
+@onready var window_sprite := $Sprite2D
 
 var player = null
 var effect
@@ -10,17 +12,23 @@ var effect_happened := false
 
 signal play_effect
 
+# Centers window on tile so it fits neatly with the walls
+func _ready():
+	position = get_parent().map_to_local(get_parent().local_to_map(position))
+
 func _on_interact_area_body_entered(body):
 	if "collision_layer" not in body:
 		return
 	if body.collision_layer == 6:
 		play_effect.emit(body.get_parent())
 		window_sfx.play()
+		window_sprite.texture = broken_window_sprite
 		body.get_parent().get_parent().remove_child(body.get_parent())
-	else:
+	elif body.collision_layer == 5:
 		player = body
 		player.nearby_objects_with_descriptions.append(self)
 
 func _on_interact_area_body_exited(_body):
-	player.nearby_objects_with_descriptions.erase(self)
-	player = null
+	if player != null:
+		player.nearby_objects_with_descriptions.erase(self)
+		player = null
