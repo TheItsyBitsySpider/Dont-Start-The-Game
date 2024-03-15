83b979e8852373039338d65103630986d2b3f998
Azia Bay-Asen <aziabayasen@gmail.com>
2024-03-14 19:10:50 -0500

    feat: level completed sfx

--- DontStartTheGame/Scripts/Levels/Elevator.gd
+++ DontStartTheGame/Scripts/Levels/Elevator.gd
@@ -1,13 +1,17 @@
 extends Area2D
 
+var is_locked := true
+
 func _ready():
 	lock_elevator()
 	body_entered.connect(get_parent().change_level)
 
 func lock_elevator():
+	is_locked = true
 	set_deferred("monitorable", false)
 	set_deferred("monitoring", false)
 	
 func unlock_elevator():
+	is_locked = false
 	set_deferred("monitorable", true)
 	set_deferred("monitoring", true)
