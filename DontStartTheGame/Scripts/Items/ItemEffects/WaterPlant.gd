f28d4f5e2a1f8d355b42531432b74f484dbd3452
Azia Bay-Asen <aziabayasen@gmail.com>
2024-03-15 18:37:20 -0500

    feat: plant object
    Note: Core of floor two is complete.

--- DontStartTheGame/Scripts/Items/ItemEffects/WaterPlant.gd
+++ DontStartTheGame/Scripts/Items/ItemEffects/WaterPlant.gd
@@ -1,18 +1,22 @@
 extends Node2D
 
 @export var required_item: PackedScene
-@export var plant_sprite: Sprite2D
+var overwatered := false
 
 signal play_effect
+signal overwater
 
 func _ready():
 	get_parent().effect = water_plant
 	get_parent().effect_node = self
-	
+
 func water_plant(player):
 	if required_item != null \
 	and player.item_held != null \
 	and player.item_held.scene_file_path == required_item.resource_path:
 		player.use_item(player.item_held)
-		plant_sprite.modulate = Color(0, 0, 255)
-		play_effect.emit()
+		if not overwatered:
+			overwater.emit()
+		else:
+			play_effect.emit()
+		overwatered = true
--- DontStartTheGame/Scripts/Objects/InteractableObject.gd
+++ DontStartTheGame/Scripts/Objects/InteractableObject.gd
@@ -1,12 +1,12 @@
 extends StaticBody2D
 
 @export var normal_sprite: Resource
-@export var spilled_sprite_1: Resource
-@export var spilled_sprite_2: Resource
-@export var spilled_sprite_3: Resource
-@export var splash_sfx: Resource
+@export var special_sprite_1: Resource
+@export var special_sprite_2: Resource
+@export var special_sprite_3: Resource
+@export var normal_sfx: Resource
+@export var item_effect: Node2D
 @onready var sprite := $Sprite2D
-@onready var give_item_effect := $GiveItemEffect
 @onready var popup_node := $Popup
 
 var player = null
@@ -16,39 +16,48 @@ var effect_node
 signal play_effect
 signal play_spill_effect
 
+func _process(_delta):
+	if item_effect != null:
+		popup_node.visible = false
+	if player != null and player.item_held != null and item_effect != null \
+	and player.item_held.scene_file_path == item_effect.required_item.resource_path:
+		popup_node.visible = true
+
 func _on_interact_area_body_entered(body):
 	if body is CharacterBody2D:
 		player = body
 		player.nearby_interactables.append(self)
-		popup_node.visible = true
+		if item_effect == null:
+			popup_node.visible = true
 
 func _on_interact_area_body_exited(body):
 	if body is CharacterBody2D:
 		player.nearby_interactables.erase(self)
 		player = null
-		popup_node.visible = false
+		if item_effect == null:
+			popup_node.visible = false
 
 func _on_give_item_effect_play_effect():
-	_play_splash_sfx()
+	_play_normal_sfx()
 	play_effect.emit()
 
-func spill():
+func cycle():
 	var sprite_list := [
 		normal_sprite,
-		spilled_sprite_1,
-		spilled_sprite_2,
-		spilled_sprite_3
+		special_sprite_1,
+		special_sprite_2,
+		special_sprite_3
 	]
 	var i := sprite_list.find(sprite.texture)
-	if i + 1 < len(sprite_list):
+	if i + 1 < len(sprite_list) and sprite_list[i + 1] != null:
 		sprite.texture = sprite_list[i + 1]
-	_play_splash_sfx()
+	_play_normal_sfx()
 	play_effect.emit()
 	play_spill_effect.emit()
 
-func _play_splash_sfx():
+func _play_normal_sfx():
 	var sfx := AudioStreamPlayer.new()
 	add_child(sfx)
-	sfx.set_stream(splash_sfx)
+	sfx.set_stream(normal_sfx)
 	sfx.play()
 	sfx.connect("finished", sfx.queue_free)
