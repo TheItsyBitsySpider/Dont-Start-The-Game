50b38fdae05dc01d2e1f1e80ad6cbdce0941f0fc
TheItsyBitsySpider <k2intern1@madarhome.com>
2024-03-17 13:52:41 -0500

    feat: Rulebook Object
    Added in a rulebook object. The sprite  still uses the safe object sprite, so that will need to be replaced

---
+++ DontStartTheGame/Scripts/Effects/WriteInRulebookEffect.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+@export var required_item: PackedScene
+
+var unwritten := true
+
+signal play_effect
+
+func _ready():
+	get_parent().effect = give_item_effect
+	get_parent().effect_node = self
+	
+func give_item_effect(player):
+	if required_item != null \
+	and player.item_held != null \
+	and player.item_held.scene_file_path == required_item.resource_path \
+	and unwritten:
+		play_effect.emit()
+		unwritten = false
+	return false
